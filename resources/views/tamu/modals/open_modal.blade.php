7c619f80018c07e3e113eb5fe5b09949c934352f
cholid15 <cholidfajar15@gmail.com>
2025-06-26 11:03:32 +0700

    menambahkan halaman depan tamu digital, membuat modal masing masing isian tamu  & custom css.

--- resources/views/tamu/modals/open_modal.blade.php
+++ resources/views/tamu/modals/open_modal.blade.php
@@ -1,46 +1,86 @@
+<style>
+    .row {
+        gap: 1.5rem;
+        /* Jarak antar kartu */
+        flex-wrap: wrap;
+        /* Biar bisa pindah baris kalau sempit */
+    }
+
+    .card-guest {
+        width: 100%;
+        max-width: 260px;
+    }
+</style>
+
 <div class="container mt-5">
-    <div class="row">
+    <div class="row d-flex justify-content-center align-items-center ">
         {{-- Tamu Internal --}}
-        <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card card-default">
-                <div class="card-header card-header-border-bottom">
-                    <h2>Tamu Internal</h2>
-                </div>
-                <div class="card-body">
-                    <button type="button" class="btn btn-info btn-pill" data-toggle="modal" data-target="#tamuInternal">
-                        Demo Modal
-                    </button>
-                </div>
+
+        <div class="card card-default card-guest">
+            <div class="card-header card-header-border-bottom d-flex flex-column align-items-center">
+                <!-- SVG -->
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
+                    class="bi bi-person-fill-down" viewBox="0 0 16 16">
+                    <path
+                        d="M12.5 9a3.5 3.5 0 1 1 0 7 3.5 3.5 0 0 1 0-7m.354 5.854 1.5-1.5a.5.5 0 0 0-.708-.708l-.646.647V10.5a.5.5 0 0 0-1 0v2.793l-.646-.647a.5.5 0 0 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                    <path
+                        d="M2 13c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4" />
+                </svg>
+
+            </div>
+
+            <div class="card-body text-center">
+                <button type="button" class="btn btn-info btn-pill" data-toggle="modal" data-target="#tamuInternal">
+                    Isi Data
+                </button>
             </div>
         </div>
+
+
+
 
         {{-- Tamu Eksternal --}}
-        <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card card-default">
-                <div class="card-header card-header-border-bottom">
-                    <h2>Tamu Luar</h2>
-                </div>
-                <div class="card-body">
-                    <button type="button" class="btn btn-info btn-pill" data-toggle="modal"
-                        data-target="#tamuEksternal">
-                        Demo Modal
-                    </button>
-                </div>
+
+        <div class="card card-default  card-guest">
+            <div class="card-header card-header-border-bottom d-flex flex-column align-items-center">
+                <!-- SVG -->
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
+                    class="bi bi-person-fill-up" viewBox="0 0 16 16">
+                    <path
+                        d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.354-5.854 1.5 1.5a.5.5 0 0 1-.708.708L13 11.707V14.5a.5.5 0 0 1-1 0v-2.793l-.646.647a.5.5 0 0 1-.708-.708l1.5-1.5a.5.5 0 0 1 .708 0M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                    <path
+                        d="M2 13c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4" />
+                </svg>
+            </div>
+
+            <div class="card-body text-center">
+                <button type="button" class="btn btn-info btn-pill" data-toggle="modal" data-target="#tamuEksternal">
+                    Isi Data
+                </button>
             </div>
         </div>
 
+
         {{-- Tamu Mahasiswa --}}
-        <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card card-default">
-                <div class="card-header card-header-border-bottom">
-                    <h2>Tamu Mahasiswa</h2>
-                </div>
-                <div class="card-body">
-                    <button type="button" class="btn btn-info btn-pill" data-toggle="modal" data-target="#tamuMhs">
-                        Demo Modal
-                    </button>
-                </div>
+
+        <div class="card card-default  card-guest">
+            <div class="card-header card-header-border-bottom d-flex flex-column align-items-center">
+                <!-- SVG -->
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
+                    class="bi bi-mortarboard-fill" viewBox="0 0 16 16">
+                    <path
+                        d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917z" />
+                    <path
+                        d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466z" />
+                </svg>
+            </div>
+            <div class="card-body text-center">
+                <button type="button" class="btn btn-info btn-pill" data-toggle="modal" data-target="#tamuMhs">
+                    Isi Data
+                </button>
             </div>
         </div>
+
+
     </div>
 </div>
--- resources/views/tamu/view.blade.php
+++ resources/views/tamu/view.blade.php
@@ -7,42 +7,51 @@
 
     <!-- MONO CSS -->
     <link id="main-css-href" rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="{{ asset('css/myCustom.css') }}" />
     <link href="images/favicon.ico" rel="shortcut icon" />
-    <link rel="stylesheet" href="css/myCustom.css" />
+
 
 
 </head>
 
 <body>
-    <div class="d-flex flex-column justify-content-center align-items-center  text-center">
-        <!-- Gambar -->
-        <img src="{{ asset('images/bukutamu/logoybwsa.png') }}" alt="Logo YBWSA" class="mt-2 img-fluid"
-            style="max-width: 85px;">
+    <div class="wrapper d-flex flex-column justify-content-between min-vh-100">
 
+        {{-- Header Info --}}
+        <div class="d-flex flex-column justify-content-center align-items-center text-center mt-3">
+            <!-- Gambar -->
+            <img src="{{ asset('images/bukutamu/logoybwsa.png') }}" alt="Logo YBWSA" class="mt-2 img-fluid"
+                style="max-width: 85px;">
+            <!-- Teks utama -->
+            <h1 class="mt-3 mb-3">Buku Tamu Digital YBWSA</h1>
+            <!-- Tanggal -->
+            <h2>Senin, 24 Juni 2025</h2>
+            <!-- Jam -->
+            <h3>13:45</h3>
+        </div>
 
-        <!-- Teks utama -->
-        <h1 class="mt-3">Buku Tamu Digital YBWSA</h1>
-        <!-- Tanggal -->
-        <h2>Senin, 24 Juni 2025</h2>
-        <!-- Jam -->
-        <h3>13:45:30</h3>
+        {{-- modal open --}}
+        @include('tamu.modals.open_modal')
+
+        {{-- isi modal --}}
+        @include('tamu.modals.modal_internal')
+        @include('tamu.modals.modal_eksternal')
+        @include('tamu.modals.modal_mhs')
+
+        {{-- Informasi Tambahan --}}
+        <div class="text-center mt-4">
+            <p class="text-muted">Belum ada tamu/kunjungan hari ini.</p>
+        </div>
+
+        {{-- Footer --}}
+        <footer class="text-center py-3">
+            <small class="text-muted">YBWSA © {{ date('Y') }}</small>
+        </footer>
+
     </div>
-
-
-
-
-    {{-- modal open --}}
-    @include('tamu.modals.open_modal')
-
-
-    {{-- isi modal --}}
-    @include('tamu.modals.modal_internal')
-    @include('tamu.modals.modal_eksternal')
-    @include('tamu.modals.modal_mhs')
-
-
-
 </body>
+
+
 <script src="plugins/jquery/jquery.min.js"></script>
 <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
